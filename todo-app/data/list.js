387385245d3059bbf3300186ff0231de3d784339
Khan <mdkhan@internal.tavisca.com>
2019-08-21 17:55:12 +0530

    Added some fake data to show as list

---
+++ todo-app/data/list.js
@@ -0,0 +1,26 @@
+const list = [
+    {
+        title: "Wakeup",
+        body: "jhqwgdj  iquiu  iuhqiuef uihqiuef ewjhhf",
+        canDelete: true,
+        canUpdate: true
+    },
+    {
+        title: "Breakfast",
+        body: "jhqwgdj  iquiu  iuhqiuef uihqiuef ewjhhf",
+        canDelete: true,
+        canUpdate: true
+    },
+    {
+        title: "Bath",
+        body: "jhqwgdj  iquiu  iuhqiuef uihqiuef ewjhhf",
+        canDelete: true,
+        canUpdate: true
+    },
+    {
+        title: "Walk",
+        body: "jhqwgdj  iquiu  iuhqiuef uihqiuef ewjhhf",
+        canDelete: true,
+        canUpdate: true
+    }
+];
