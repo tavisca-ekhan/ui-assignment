2f7486c02e8f3d425d6095a9ce4e85446f3083a7
Khan <mdkhan@internal.tavisca.com>
2019-08-22 23:52:30 +0530

    Search Part is incomplete

---
+++ todo-app/src/data.js
@@ -0,0 +1,26 @@
+const lists = [
+    {
+        title: "Go for a run"
+    },
+    {
+        title: "Take a shower"
+    },
+    {
+        title: "Do Breakfast"
+    },
+    {
+        title: "Learn GraphQL"
+    },
+    {
+        title: "Practice Node.js"
+    },
+    {
+        title: "Sutta Time"
+    },
+    {
+        title: "Music On!!!!!!!!!!!!!!!!!!!"
+    },
+    {
+        title: "Tea Break, Yeah!!!!!!!!"
+    }
+]
